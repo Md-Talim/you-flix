68cf23985eb7c65987469015f407333797eff135
Mohammad Talim <talimmohammad116@gmail.com>
2023-12-14 21:57:37 +0530

    Build a footer component

---
+++ app/Footer.tsx
@@ -0,0 +1,26 @@
+import Logo from './Logo';
+
+const Footer = () => (
+  <footer className="border-t text-zinc-400 border-zinc-800 mt-10 py-8 space-y-6 px-16">
+    <div className="">
+      <Logo />
+    </div>
+
+    <div className="flex items-center justify-between">
+      <p>
+        Built by <a href="https://linkedin.com/in/Md-Talim">Md. Talim</a>
+      </p>
+
+      <nav>
+        <ul className="flex items-center gap-x-4">
+          <li>Repository</li>
+          <li>LinkedIn</li>
+          <li>Twitter</li>
+          <li>Copy Email</li>
+        </ul>
+      </nav>
+    </div>
+  </footer>
+);
+
+export default Footer;
